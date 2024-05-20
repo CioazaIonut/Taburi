0b3719c29c33eed3b8d6020b46226ec03b80ce49
CioazaIoan <cioazai@yahoo.com>
2024-05-20 18:56:08 +0300

    nu functioneaza see more

--- script.js
+++ script.js
@@ -5,13 +5,23 @@ const primulPraragraf = document.querySelector(".primul-paragraf");
 const alDoileaParagraf = document.querySelector(".al-doilea-paragraf");
 const alTreileaParagraf = document.querySelector(".al-treilea-paragraf");
 const resetButton = document.querySelector('.reset-button');
+const seeMoreUnu = document.querySelector(".content-more-unu");
+const spanMoreUnu = document.querySelector(".lorem-content-unu");
+const seeMoreDoi = document.querySelector(".content-more-doi");
+const spanMoreDoi = document.querySelector(".lorem-content-doi");
+const seeMoreTrei = document.querySelector(".content-more-trei");
+const spanMoreTrei = document.querySelector(".lorem-content-trei");
 
 
 //primul tab
 const deschidePrimulTab = () => {
+    primulPraragraf.style.display = "block"
     alDoileaParagraf.style.display = "none"
     alTreileaParagraf.style.display = "none"
-    primulPraragraf.style.display = "block"
+
+    primulTab.classList.add("active-tab");
+    alDoileaTab.classList.remove("active-tab");
+    alTreileaTab.classList.remove("active-tab");
     //alert("test")
 }
 
@@ -21,8 +31,12 @@ primulTab.addEventListener("click", deschidePrimulTab);
 //al dolea tab
 const deschidealDoileaTab = () => {
     primulPraragraf.style.display = "none"
-    alTreileaParagraf.style.display = "none"
     alDoileaParagraf.style.display = "block"
+    alTreileaParagraf.style.display = "none"
+
+    primulTab.classList.remove("active-tab");
+    alDoileaTab.classList.add("active-tab");
+    alTreileaTab.classList.remove("active-tab");
 }
 
 alDoileaTab.addEventListener("click", deschidealDoileaTab);
@@ -33,15 +47,37 @@ const deschidealTreileaTab = () => {
     primulPraragraf.style.display = "none"
     alDoileaParagraf.style.display = "none"
     alTreileaParagraf.style.display = "block"
+
+    primulTab.classList.remove("active-tab");
+    alDoileaTab.classList.remove("active-tab");
+    alTreileaTab.classList.add("active-tab");
 }
 
 alTreileaTab.addEventListener("click", deschidealTreileaTab);
 
+
+// buton reset
+
 const reset = () => {
-    // nu imi dau seama ce sa scriu in functie
+    primulPraragraf.style.display = "block"
+    alDoileaParagraf.style.display = "none"
+    alTreileaParagraf.style.display = "none"
+
+    primulTab.classList.add("active-tab");
+    alDoileaTab.classList.remove("active-tab");
+    alTreileaTab.classList.remove("active-tab");
 };
 
-resetButton.addEventListener('click');
+resetButton.addEventListener('click', reset);
+
+//see more Button
+
+const seeMoreFunction = () =>{
+    spanMoreUnu.classList.remove("lorem-content-unu");
+
+}
+
+seeMore.addEventListener ("click", seeMoreFunction);
 /*
 structura generala a unui addEventListener:
 1: scriem numele variabileiin care ne-am selectat butonul
@@ -53,4 +89,11 @@ structura generala a unui addEventListener:
 
 /* 
 cand in consola vedem eroarea "ceva is not defined" inseamna ca noi nu avem nici o contanta / variabila cu numele ceva
+*/
+
+/*
+TEMA !!!!!!!!!!!!!!!!!
+1 cand apas pe reset sa dispara see more
+2 de facut pt restul taburilor
+3 de facut un buton de See Less
 */
